bbc60a0e36bee37b54ecc3a9fb81cd5114d3aa12
79671505646 <116814182+79671505646@users.noreply.github.com>
2022-11-01 23:10:48 +0300

    Add files via upload
    1 задача

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2){
+    Console.WriteLine(num1);
+}
+else{
+    Console.WriteLine(num2);
+}
