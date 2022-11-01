24252a8f6d62f87b999428d6adabc8e3de824c56
Ilya <79671505646@ya.ru>
2022-11-01 23:00:23 +0300

    add Dz 3

---
+++ GB_3499/C#_DZ_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 3 числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ((num1>num2)&&(num1>num3)){
+    max = num1 ;
+}
+else if ((num2>num1)&&(num2>num3)){
+    max = num2 ;
+}
+else if ((num3>num1)&&(num3>num2)){
+    max = num3 ;}
+Console.WriteLine(max);
+Console.WriteLine();
